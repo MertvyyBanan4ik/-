daaf623ab097233781c504c84abb6863711157c4
MertvyyBanan4ik <78962662+MertvyyBanan4ik@users.noreply.github.com>
2021-02-12 16:23:48 +0300

    Add files via upload

---
+++ kalkulator/practice-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.калькуляторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.разработчикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.калькуляторToolStripMenuItem,
+            this.разработчикToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1548, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // калькуляторToolStripMenuItem
+            // 
+            this.калькуляторToolStripMenuItem.Name = "калькуляторToolStripMenuItem";
+            this.калькуляторToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.калькуляторToolStripMenuItem.Text = "Калькулятор";
+            this.калькуляторToolStripMenuItem.Click += new System.EventHandler(this.калькуляторToolStripMenuItem_Click);
+            // 
+            // разработчикToolStripMenuItem
+            // 
+            this.разработчикToolStripMenuItem.Name = "разработчикToolStripMenuItem";
+            this.разработчикToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.разработчикToolStripMenuItem.Text = "Разработчик";
+            this.разработчикToolStripMenuItem.Click += new System.EventHandler(this.разработчикToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1548, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem калькуляторToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem разработчикToolStripMenuItem;
+    }
+}
+
---
+++ kalkulator/practice-master/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 f2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void калькуляторToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            f2 = new Form2();
+            f2.Show();
+        }
+
+        private void разработчикToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Загидулин Радмир, 881 группа");
+        }
+    }
+}
